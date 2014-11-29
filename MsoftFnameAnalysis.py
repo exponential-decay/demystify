5faf4351dd74ed4538f4e2b0506e40175757864d
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2014-11-29 18:21:14 +1300

    [feature] Verbose mode for library for full filename output.

--- MsoftFnameAnalysis.py
+++ MsoftFnameAnalysis.py
@@ -9,12 +9,15 @@ class MsoftFnameAnalysis:
 
    report = ''
 
-   def completeFnameAnalysis(self, s):
+   #Note: verbose default false because of potential no. characters in a collection
+   def completeFnameAnalysis(self, s, verbose=False):
       #need to ensure argument is interpreted correctly as UNICODE on way in
       #without explicit decode we have str type
       s = s.decode('utf8')
-
-      self.report = u""
+  
+      self.report= u""
+      self.verbose = verbose
+      
       self.detectNonAsciiCharacters(s)
       self.detectNonRecommendedCharacters(s)
       self.detectNonPrintableCharacters(s)
@@ -23,37 +26,41 @@ class MsoftFnameAnalysis:
       self.detectPeriodAtEndOfName(s)
       return self.report
 
+   def reportIssue(self, s, msg, value=''):
+      self.report = self.report + "File: " + s + " " + msg + " " + value + "\n"
+
    def unicodename(self, c):
       ref = names.lookup()
       name = ref.name(c)
       return "%s: %s" % (name, c)
 
    def detectNonAsciiCharacters(self, s):
-      print type(s)
       #Nicer method: all(ord(c) < 128 for c in s)
       nonascii = False
-      char = ""
       for c in s:
          if ord(c) > 128:
             nonascii = True
-            char = char + c
-            break
-      if nonascii == True:
-         print type(char)
-         self.reportIssue(s, "contains, characters outside of ASCII range:", hex(ord(char)) + ", " + self.unicodename(char))
+            if nonascii == True:
+               self.reportIssue(s, "contains, characters outside of ASCII range:", hex(ord(c)) + ", " + self.unicodename(c))
+               nonascii == False
+            if self.verbose == False:
+               break
+            
 
    def detectNonRecommendedCharacters(self, s):
       charlist = ['<','>',':','"','/','\\','?','*','|', ']', '[']
-      for c in charlist:
-         if c in s:
+      for c in s:
+         if c in charlist:
             self.reportIssue(s, "contains, non-recommended character:", hex(ord(c)) + ", " + self.unicodename(c))
-            break
+            if self.verbose == False:
+               break
 
    def detectNonPrintableCharacters(self, s):
       for c in range(0x1f):
          if chr(c) in s:
             self.reportIssue(s, "contains, non-printable character:", hex(c) + ", " + self.unicodename(c))
-            break
+            if self.verbose == False:
+               break
 
    def detectMsoftReservedNames(self, s):
       badnames = ['CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', \
@@ -81,10 +88,7 @@ class MsoftFnameAnalysis:
    def detectPeriodAtEndOfName(self, s):		
       if s.endswith('.'):
          self.reportIssue(s, "has a period as its last character.")
-	
-   def reportIssue(self, s, msg, value=''):
-      self.report = "File: " + s + " " + msg + " " + value + "\n"
-	
+		
    def __detect_invalid_characters_test__(self):
       #Strings for unit tests
       test_strings = ['COM4', 'COM4.txt', '.com4', 'abcCOM4text', 'abc.com4.txt.abc', 'con', 'CON', 'consumer', 'space ', 'preiod.', 'ף', 'י', 'צ', 'ףיצ', 'file[bracket]one.txt', 'file[two.txt', 'filethree].txt', '-=_|\"', '(<|>|:|"|/|\\|\?|\*|\||\x00-\x1f)']	
