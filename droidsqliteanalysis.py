575153463368ca47453db006fbb128a9b83252e4
ross-spencer <all.along.the.watchtower2001@gmail.com>
2015-02-20 13:56:07 +1300

    [bugfix] Handling HTML flags the right way. True not False.

--- droidsqliteanalysis.py
+++ droidsqliteanalysis.py
@@ -6,26 +6,30 @@ import sys
 import droid2sqlite
 import htmloutputclass
 import textoutputclass
+import roguesgalleryoutputclass
 import DroidAnalysisClass
 
-def handleOutput(analysisresults, htmlout=False):
-   if htmlout is False:
+def handleOutput(analysisresults, htmlout=False, rogues=False):
+   if htmlout is True:
       htmloutput = htmloutputclass.DROIDAnalysisHTMLOutput(analysisresults)
       sys.stdout.write(htmloutput.printHTMLResults())   
+   #elif rogues is True:
+   #   rogueoutput = roguesgalleryoutputclass.rogueoutputclass(analysisresults)
+   #   rogueoutput.printTextResults()
    else:
       textoutput = textoutputclass.DROIDAnalysisTextOutput(analysisresults)
       textoutput.printTextResults() # Text class still uses print statements... 
 
-def handleDROIDDB(dbfilename, htmlout=False):
+def handleDROIDDB(dbfilename, htmlout=False, rogues=False):
    analysis = DroidAnalysisClass.DROIDAnalysis()	
    analysisresults = analysis.openDROIDDB(dbfilename)
    return analysisresults
 
-def handleDROIDCSV(droidcsv, analyse=False, htmlout=False):
+def handleDROIDCSV(droidcsv, analyse=False, htmlout=False, rogues=False):
    dbfilename = droid2sqlite.handleDROIDCSV(droidcsv)
    if analyse == True:
       analysisresults = handleDROIDDB(dbfilename)
-      handleOutput(analysisresults, htmlout)
+      handleOutput(analysisresults, htmlout, rogues)
 
 def main():
 
@@ -36,7 +40,8 @@ def main():
    parser.add_argument('--csv', help='Optional: Single DROID CSV to read.', default=False)
    parser.add_argument('--csva', help='Optional: DROID CSV to read, and then analyse.', default=False)
    parser.add_argument('--db', help='Optional: Single DROID sqlite db to read.', default=False)
-   parser.add_argument("--htm", "--html", help="Output HTML instead of text.", action="store_false")
+   parser.add_argument("--htm", "--html", help="Output HTML instead of text.", action="store_true")
+   parser.add_argument("--rogues", "--rogue", help="Output 'Rogues Gallery' listing.", action="store_true")
 
    if len(sys.argv)==1:
       parser.print_help()
@@ -49,9 +54,9 @@ def main():
    if args.csv:
       handleDROIDCSV(args.csv)
    if args.csva:
-      handleDROIDCSV(args.csva, True, args.htm)
+      handleDROIDCSV(args.csva, True, args.htm, args.rogues)
    if args.db:
-      handleDROIDDB(args.db, args.htm)
+      handleDROIDDB(args.db, args.htm, args.rogues)
    
    else:
       sys.exit(1)
