8110f2a12665e8a017fbafd84bca27f4b9172a71
ross-spencer <all.along.the.watchtower2001@gmail.com>
2015-02-24 16:57:17 +1300

    [feature] Rogues gallery feature added by byrnean.

--- roguesgalleryoutputclass.py
+++ roguesgalleryoutputclass.py
@@ -8,12 +8,15 @@ class rogueoutputclass:
 
    def printTextResults(self):
 
-   	print self.analysisresults.multipleIDList
-   	print self.analysisresults.filesWithNoIDList
-   	print self.analysisresults.extensionOnlyIDList
-   	print self.analysisresults.extmismatchList 
-   	print self.analysisresults.zerobytelist
-   	print self.analysisresults.duplicatemd5altlisting
-   	print self.analysisresults.duplicatefnamealtlisting
-   	print self.analysisresults.multiplespacelist
+      print self.analysisresults.multipleIDList
+      print self.analysisresults.filesWithNoIDList
+      print self.analysisresults.extensionOnlyIDfnameList
+      print self.analysisresults.extmismatchList 
+      print self.analysisresults.zerobytelist
+      for x in self.analysisresults.duplicatemd5altlisting:
+        print x
+      for d in self.analysisresults.duplicatefnamealtlisting:
+         print d
+      for f in self.analysisresults.multiplespacelist:
+         print "\\".join(f)
 
