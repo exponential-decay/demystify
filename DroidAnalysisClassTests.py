78c13f813c010e79fc8685fdcf877ff5fb0dc1b1
ross-spencer <all.along.the.watchtower2001@gmail.com>
2015-08-20 13:20:34 +1200

    [testing] Creating harness for unit testing.

--- DroidAnalysisClassTests.py
+++ DroidAnalysisClassTests.py
@@ -1,3 +1,22 @@
 #Test list of duplicates returned. (count, values)
 #Test rogues paths are only paths to files, and not paths to directories
 #Test config options are working
+
+from unittest import TestCase, TestLoader, TextTestRunner
+from DroidAnalysisClass import *
+import testdata
+
+class DroidAnalysisClassTests(TestCase):
+
+   def setup(self):
+      self.empty = DetectEmpties()
+   
+   def testRecurseDeleteFolders(self):
+      print "test todo"   
+
+def main():
+	suite = TestLoader().loadTestsFromTestCase(DroidAnalysisClassTests)
+	TextTestRunner().run(suite)
+	
+if __name__ == "__main__":
+	main()
