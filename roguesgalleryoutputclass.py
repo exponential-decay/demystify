a81dc91b1660d3e922f7b445088019b6f05f590e
ross-spencer <allalongthewatchtower2001@gmail.com>
2015-08-21 14:49:30 +1200

    [feature] Heros infrastructure.

--- roguesgalleryoutputclass.py
+++ roguesgalleryoutputclass.py
@@ -3,20 +3,34 @@ import DroidAnalysisClass
 
 class rogueoutputclass:
 
+   herolist = []
+
    def __init__(self, analysisresults, heros=False):
       self.analysisresults = analysisresults
+      self.heros = heros
 
    def outputlist(self, pathlist):
-      if pathlist != False:
-         for x in pathlist:
-            if x != "no value":
-               sys.stdout.write(x + "\n")
+      for x in pathlist:
+         if x != "no value":
+            sys.stdout.write(x + "\n")
+            
+   def rogueorhero(self, pathlist):
+      if self.heros == False:
+         if pathlist != False:
+            self.outputlist(pathlist)
+      else:
+         #heros list to become direct opposite of rogues list
+         if pathlist != False:
+            self.herolist = self.herolist + pathlist
 
    def printTextResults(self):
 
-      self.outputlist(self.analysisresults.multipleIDList)
-      self.outputlist(self.analysisresults.filesWithNoIDList)
-      self.outputlist(self.analysisresults.extensionOnlyIDfnameList)
-      self.outputlist(self.analysisresults.extmismatchList) 
-      self.outputlist(self.analysisresults.zerobytelist)
-      self.outputlist(self.analysisresults.duplicatemd5pathlisting)
+      self.rogueorhero(self.analysisresults.multipleIDList)
+      self.rogueorhero(self.analysisresults.filesWithNoIDList)
+      self.rogueorhero(self.analysisresults.extensionOnlyIDfnameList)
+      self.rogueorhero(self.analysisresults.extmismatchList) 
+      self.rogueorhero(self.analysisresults.zerobytelist)
+      self.rogueorhero(self.analysisresults.duplicatemd5pathlisting)
+      
+      if self.heros is True:
+         self.outputlist(self.herolist)
