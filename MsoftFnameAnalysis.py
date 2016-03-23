b7d6b9518d369dc217ce20f610d81e701c8fd50e
ross-spencer <allalongthewatchtower2001@gmail.com>
2016-03-23 19:13:39 +1300

    [feature] Improving directory handling.

--- MsoftFnameAnalysis.py
+++ MsoftFnameAnalysis.py
@@ -22,30 +22,34 @@ class MsoftFnameAnalysis:
       self.report= u""
       self.verbose = verbose
       
-      self.detectNonAsciiCharacters(s)
+      self.detectNonAsciiCharacters(s, folders)
       self.detectNonRecommendedCharacters(s, folders)
-      self.detectNonPrintableCharacters(s)
-      self.detectMsoftReservedNames(s)
-      self.detectSpaceAtEndOfName(s)
-      self.detectPeriodAtEndOfName(s)
+      self.detectNonPrintableCharacters(s, folders)
+      self.detectMsoftReservedNames(s, folders)
+      self.detectSpaceAtEndOfName(s, folders)
+      self.detectPeriodAtEndOfName(s, folders)
       return self.report
 
-   def reportIssue(self, s, msg, value=''):
-      self.report = self.report + "File: " + s + " " + msg + " " + value + "\n"
+   def reportIssue(self, s, msg, value='', folders=False):
+      if folders == True:
+         text = 'Directory: '
+      else:
+         text = 'File: '
+      self.report = self.report + text + s + " " + msg + " " + value + "\n"
 
    def unicodename(self, c):
       ref = names.lookup()
       name = ref.name(c)
       return "%s: %s" % (name, c)
 
-   def detectNonAsciiCharacters(self, s):
+   def detectNonAsciiCharacters(self, s, folders=False):
       #Nicer method: all(ord(c) < 128 for c in s)
       nonascii = False
       for c in s:
          if ord(c) > 128:
             nonascii = True
             if nonascii == True:
-               self.reportIssue(s, self.STRINGS.FNAME_CHECK_ASCII + ":", hex(ord(c)) + ", " + self.unicodename(c))
+               self.reportIssue(s, self.STRINGS.FNAME_CHECK_ASCII + ":", hex(ord(c)) + ", " + self.unicodename(c), folders)
                nonascii == False
             if self.verbose == False:
                break
@@ -57,18 +61,18 @@ class MsoftFnameAnalysis:
          
       for c in s:
          if c in charlist:
-            self.reportIssue(s, self.STRINGS.FNAME_CHECK_NOT_RECOMMENDED + ":", hex(ord(c)) + ", " + self.unicodename(c))
+            self.reportIssue(s, self.STRINGS.FNAME_CHECK_NOT_RECOMMENDED + ":", hex(ord(c)) + ", " + self.unicodename(c), folders)
             if self.verbose == False:
                break
 
-   def detectNonPrintableCharacters(self, s):
+   def detectNonPrintableCharacters(self, s, folders=False):
       for c in range(0x1f):
          if chr(c) in s:
-            self.reportIssue(s, self.STRINGS.FNAME_CHECK_NON_PRINT + ":", hex(c) + ", " + self.unicodename(c))
+            self.reportIssue(s, self.STRINGS.FNAME_CHECK_NON_PRINT + ":", hex(c) + ", " + self.unicodename(c), folders)
             if self.verbose == False:
                break
 
-   def detectMsoftReservedNames(self, s):
+   def detectMsoftReservedNames(self, s, folders=False):
       badnames = ['CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', \
                      'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9', \
                         'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', \
@@ -85,15 +89,15 @@ class MsoftFnameAnalysis:
             except IndexError:
                problem = True
             if problem == True:
-               self.reportIssue(s, self.STRINGS.FNAME_CHECK_RESERVED + ": ", c)
+               self.reportIssue(s, self.STRINGS.FNAME_CHECK_RESERVED + ": ", c, folders)
 
-   def detectSpaceAtEndOfName(self, s):
+   def detectSpaceAtEndOfName(self, s, folders=False):
       if s.endswith(' '):
-         self.reportIssue(s, self.STRINGS.FNAME_CHECK_SPACE + ".")
+         self.reportIssue(s, self.STRINGS.FNAME_CHECK_SPACE + ".", folders)
          
-   def detectPeriodAtEndOfName(self, s):		
+   def detectPeriodAtEndOfName(self, s, folders):		
       if s.endswith('.'):
-         self.reportIssue(s, self.STRINGS.FNAME_CHECK_PERIOD + ".")
+         self.reportIssue(s, self.STRINGS.FNAME_CHECK_PERIOD + ".", folders=False)
       
    def __detect_invalid_characters_test__(self):
       #Strings for unit tests
