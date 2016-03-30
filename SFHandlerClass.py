0108ae0f35490bf2bbd1c712cb8adb89fe8f8bd5
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2016-03-30 20:47:24 +1300

    [refactor] SF integration. First draft Python header.

--- SFHandlerClass.py
+++ SFHandlerClass.py
@@ -1,12 +1,21 @@
-import sys
+# -*- coding: utf-8 -*-
+# we don't import YAML handler for this 
+# as no standard PYTHON handler library
 
 class SFYaml:
    
    sectioncount = 0
+   identifiercount = 0
    
    YAMLSECTION = "---"
+   YAMLNAMESPACE = 'name'
+   YAMLDETAILS = 'details'
 
    header = {}
+
+   HEADDETAILS = 'id_details'
+   HEADNAMESPACE = 'id_namespace'
+   HEADCOUNT = 'identifier_count'
 
    def stripkey(self, line):
       line = line.strip()
@@ -24,6 +33,20 @@ class SFYaml:
       line[1] = self.stripvalue(line[1])
       return line
 
+   def headersection(self, line):
+      if line != self.YAMLSECTION:
+         line = self.handleentry(line)
+         if line[0] == self.YAMLNAMESPACE:
+            self.identifiercount+=1
+            ns = self.HEADNAMESPACE + str(self.identifiercount)
+            self.header[ns] = line[1]
+         elif line[0] == self.YAMLDETAILS:
+            details = self.HEADDETAILS + str(self.identifiercount)
+            self.header[details] = line[1]   
+            self.header[self.HEADCOUNT] = self.identifiercount
+         elif line[0] != 'identifiers':
+            self.header[line[0]] = line[1]
+
    def sfaslist(self, sfname):
       with open(sfname, 'rb') as sfile:
          for line in sfile: 
@@ -31,8 +54,6 @@ class SFYaml:
             if line == self.YAMLSECTION:
                self.sectioncount += 1 
             if self.sectioncount == 1:
-               if line != self.YAMLSECTION:
-                  line = self.handleentry(line)
-                  self.header[line[0]] = line[1]
+               self.headersection(line)
 
       print self.header
