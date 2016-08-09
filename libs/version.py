f0a634f03ffeefd4a6ad8e8f887167e76d13b9ad
ross-spencer <allalongthewatchtower2001@gmail.com>
2016-08-09 18:03:43 +1200

    [feature] Upping version no.

--- libs/version.py
+++ libs/version.py
@@ -1,6 +1,6 @@
 class AnalysisVersion:
 
-   version = '0.5.1-BETA' #need something reasonable here...
+   version = '0.6.0-BETA' #need something reasonable here...
    
    def getVersion(self):
       return self.version
