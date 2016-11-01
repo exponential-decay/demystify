bfccea48fc296b6d65fd125b8fb03eb1aa860b7b
ross-spencer <allalongthewatchtower2001@gmail.com>
2016-11-01 17:17:36 +1300

    [bugfix] #38 fixes. Back up to equivalent functionality.

--- libs/CSVHandlerClass.py
+++ libs/CSVHandlerClass.py
@@ -63,59 +63,72 @@ class genericCSVHandler():
 
             #inspect line by reading it first.
             for lineno, line in enumerate(csvfile):            
+               
+               #check line for characters we can't parse...
                line = self.checkline(line, lineno)           
-               csvreader = unicodecsv.reader(line.splitlines(), lineno)
-               for row in csvreader:
-                  if csvreader.line_num == 1:		# not zero-based index
+               
+               #create a dict to store row information
+               csv_dict = {}
+               
+               #read each line into a unicode csv reader
+               csvreader = unicodecsv.reader(line.splitlines())
+               
+               #first we need to retrieve the header information from the first line
+               if lineno == 0:
+                  for row in csvreader:
                      header_list = self.__getCSVheaders__(row)
-                     columncount = len(header_list)
-                  else:
-                     csv_dict = {}
-                     #for each column in header
-                     #note: don't need ID data. Ignoring multiple ID.
-                     for i in range(columncount):
-                        if i == FORMAT_COUNT:
-                           if row[i] != u'':
-                              count = int(row[i])
-                           else:
-                              count = 0
-
-                           csv_dict[header_list[i]] = count
-                           
-                           #exception for multiple ids
-                           if count > 1:
-                              MULTIPLE = True
-                              max_fields = len(multi_fields) * count
+                     columncount = len(header_list)               
                               
-                              #continue to put the remainder of the content into a dict
-                              format_list = row[FORMAT_COUNT+1:]
-                              format_list = format_list[:max_fields]
-
-                              while count > 0:
-                                 mfields = multi_fields
-                                 mdict = {}
-                                 for i,t in enumerate(mfields):
-                                    mdict[t] = '"' + format_list[i] + '"'
-                                 format_list = format_list[len(mfields):]
-                                 multilist.append(mdict)
-                                 count-=1
-                                 
-                              #break for loop after cycling through remainder
-                              #for loop controls regular number of columns (count)
-                              #while loop takes us the into an exception mechanism negating that
-                              break
+               for row in csvreader:
+               
+                  #for each column in header
+                  #note: don't need ID data. Ignoring multiple ID.
+                  for i in range(columncount):
+                     if i == FORMAT_COUNT:
+                        if row[i] != u'':
+                           count = int(row[i])
                         else:
+                           count = 0
+
+                        csv_dict[header_list[i]] = count
+                        
+                        #exception for multiple ids
+                        if count > 1:
+                           MULTIPLE = True
+                           max_fields = len(multi_fields) * count
+                           
+                           #continue to put the remainder of the content into a dict
+                           format_list = row[FORMAT_COUNT+1:]
+                           format_list = format_list[:max_fields]
+
+                           while count > 0:
+                              mfields = multi_fields
+                              mdict = {}
+                              for i,t in enumerate(mfields):
+                                 mdict[t] = '"' + format_list[i] + '"'
+                              format_list = format_list[len(mfields):]
+                              multilist.append(mdict)
+                              count-=1
+                              
+                           #break for loop after cycling through remainder
+                           #for loop controls regular number of columns (count)
+                           #while loop takes us the into an exception mechanism negating that
+                           break
+                     else:
+                        try:
                            csv_dict[header_list[i]] = row[i]
-
-                     #continue with exception and add new dict to primary dict
-                     if MULTIPLE == True:
-                        csv_dict[self.DICT_FORMATS] = multilist
-
-                     #add list and reset variables
-                     csvlist.append(csv_dict)
-                     MULTIPLE = False
-                     multilist = []
+                        except IndexError:
+                           sys.stderr.write("Row len too short. Cannot write row: " + str(row) + "\n")
+                           break
+                        
+                  #continue with exception and add new dict to primary dict
+                  if MULTIPLE == True:
+                     csv_dict[self.DICT_FORMATS] = multilist
                      
+                  #add list and reset variables
+                  csvlist.append(csv_dict)
+                  MULTIPLE = False 
+                            
       return csvlist
       
    def checkline(self, line, lineno):
