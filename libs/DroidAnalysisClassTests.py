8f77b5e0487221fc1c5ca0bdda0166441cc8672e
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-04-24 09:34:25 +0200

    Black formatting

--- libs/DroidAnalysisClassTests.py
+++ libs/DroidAnalysisClassTests.py
@@ -1,22 +1,26 @@
-#Test list of duplicates returned. (count, values)
-#Test rogues paths are only paths to files, and not paths to directories
-#Test config options are working
+# Test list of duplicates returned. (count, values)
+# Test rogues paths are only paths to files, and not paths to directories
+# Test config options are working
+
+from __future__ import print_function
 
 from unittest import TestCase, TestLoader, TextTestRunner
 from DroidAnalysisClass import *
 import testdata
 
-class DroidAnalysisClassTests(TestCase):
 
-   def setup(self):
-      self.empty = DetectEmpties()
-   
-   def testRecurseDeleteFolders(self):
-      print "test todo"   
+class DroidAnalysisClassTests(TestCase):
+    def setup(self):
+        self.empty = DetectEmpties()
+
+    def testRecurseDeleteFolders(self):
+        print("test todo")
+
 
 def main():
-	suite = TestLoader().loadTestsFromTestCase(DroidAnalysisClassTests)
-	TextTestRunner().run(suite)
-	
+    suite = TestLoader().loadTestsFromTestCase(DroidAnalysisClassTests)
+    TextTestRunner().run(suite)
+
+
 if __name__ == "__main__":
-	main()
+    main()
