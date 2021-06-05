b8b5ce23543273c1bb2cc7694f9f5e1e1695e7c4
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-06-05 20:01:09 +0200

    Test changes

---
+++ demystify_tests/test_droid_analysis.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+#from libs.DroidAnalysisClass import DROIDAnalysis
+
+
+def test_recurse_delete_folders():
+    print("Test TODO")
+    assert True
