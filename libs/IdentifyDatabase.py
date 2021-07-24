b60729125c55ffd74acc93e9c6b1ce8f38fd0c1d
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-07-24 14:49:49 +0200

    Update IdentifyDB for Python3

--- libs/IdentifyDatabase.py
+++ libs/IdentifyDatabase.py
@@ -1,19 +1,34 @@
 ﻿# -*- coding: utf-8 -*-
 
+"""IdentifyDatabase
+
+Enables identification of a sqlite3 database to ensure that the input
+data for the calling application is correct.
+"""
+
 
 class IdentifyDB:
+    """IdentifyDB."""
 
     SQLITE_DB = "SQLITE 3"
     UNKNOWN_DB = "unknown"
 
     # 'SQLite format 3'
-    sqlite3 = "\x53\x51\x4C\x69\x74\x65\x20\x66\x6F\x72\x6D\x61\x74\x20\x33"
+    sqlite3 = b"\x53\x51\x4C\x69\x74\x65\x20\x66\x6F\x72\x6D\x61\x74\x20\x33"
 
-    def identify_export(self, db):
-        dbid = self.UNKNOWN_DB
-        f = open(db, "rb")
+    def identify_export(self, database_path):
+        """Reads an input file and checks whether it is an sqlite3
+        database.
+
+        :param database_path: path to database to read (String)
+        :return: `True` if sqlite3 or `False` if not (Boolean)
+        """
+        try:
+            f = open(database_path, "rb")
+        except IOError:
+            return False
         db_magic = f.read(len(self.sqlite3))
         f.close()
         if db_magic == self.sqlite3:
-            dbid = self.SQLITE_DB
-        return dbid
+            return True
+        return False
