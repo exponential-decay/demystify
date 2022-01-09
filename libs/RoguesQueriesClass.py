81dd874f15f4c6d5649adbf91f271aaadc24de10
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2022-01-10 00:16:56 +0100

    Formatting

--- libs/RoguesQueriesClass.py
+++ libs/RoguesQueriesClass.py
@@ -14,65 +14,71 @@ class RogueQueries:
         """SELECT DISTINCT FILE_PATH FROM FILEDATA WHERE FILEDATA.TYPE = 'Folder'"""
     )
 
-    SELECT_EXTENSION_MISMATCHES = """SELECT DISTINCT FILEDATA.FILE_PATH
-                                    FROM IDRESULTS
-                                    JOIN FILEDATA on IDRESULTS.FILE_ID = FILEDATA.FILE_ID
-                                    JOIN IDDATA on IDRESULTS.ID_ID = IDDATA.ID_ID
-                                    AND (IDDATA.EXTENSION_MISMATCH='True')"""
+    SELECT_EXTENSION_MISMATCHES = (
+        "SELECT DISTINCT FILEDATA.FILE_PATH\n"
+        "FROM IDRESULTS\n"
+        "JOIN FILEDATA on IDRESULTS.FILE_ID = FILEDATA.FILE_ID\n"
+        "JOIN IDDATA on IDRESULTS.ID_ID = IDDATA.ID_ID\n"
+        "AND (IDDATA.EXTENSION_MISMATCH='True')"
+    )
 
     def get_pronom_identified_files(self, pro_ns):
-        PRONOM_ONLY = """SELECT DISTINCT FILEDATA.FILE_PATH
-                       FROM IDRESULTS
-                       JOIN FILEDATA on IDRESULTS.FILE_ID = FILEDATA.FILE_ID
-                       JOIN IDDATA on IDRESULTS.ID_ID = IDDATA.ID_ID
-                       WHERE (IDDATA.METHOD != 'Container' AND IDDATA.METHOD != 'Signature')
-                       AND FILEDATA.TYPE != 'Folder'
-                       AND IDDATA.NS_ID="""
-        query = PRONOM_ONLY + str(pro_ns)
+        PRONOM_ONLY = (
+            "SELECT DISTINCT FILEDATA.FILE_PATH\n"
+            "FROM IDRESULTS\n"
+            "JOIN FILEDATA on IDRESULTS.FILE_ID = FILEDATA.FILE_ID\n"
+            "JOIN IDDATA on IDRESULTS.ID_ID = IDDATA.ID_ID\n"
+            "WHERE (IDDATA.METHOD != 'Container' AND IDDATA.METHOD != 'Signature')\n"
+            "AND FILEDATA.TYPE != 'Folder'\n"
+            "AND IDDATA.NS_ID="
+        )
+        query = "{}{}".format(PRONOM_ONLY, pro_ns)
         return query
 
     def get_all_non_ids(self, ids):
-        ALL_IDS = """SELECT DISTINCT FILEDATA.FILE_PATH
-                   FROM FILEDATA
-                   WHERE FILE_ID NOT IN"""
         csv = ",".join(ids)
-        csv = "(" + csv + ")"
-        query = ALL_IDS + csv
+        csv = "({})".format(csv)
+        ALL_IDS = (
+            "SELECT DISTINCT FILEDATA.FILE_PATH\n"
+            "FROM FILEDATA\n"
+            "WHERE FILE_ID NOT IN"
+        )
+        query = "{}{}".format(ALL_IDS, csv)
         return query
 
     def count_multiple_ids(self, nscount, paths=False):
-        count = "SELECT count(FREQUENCY)" + "\n"
-        pathquery = "SELECT PATH" + "\n"
-        body = """FROM (SELECT DISTINCT FILEDATA.FILE_PATH AS PATH, COUNT(FILEDATA.FILE_ID) AS FREQUENCY
-                  FROM IDRESULTS
-                  JOIN FILEDATA on IDRESULTS.FILE_ID = FILEDATA.FILE_ID
-                  JOIN IDDATA on IDRESULTS.ID_ID = IDDATA.ID_ID
-                  WHERE (IDDATA.METHOD='Signature' or IDDATA.METHOD='Container')
-                  GROUP BY FILEDATA.FILE_ID
-                  ORDER BY COUNT(FILEDATA.FILE_ID) DESC)
-                  WHERE FREQUENCY > """
-
-        query = ""
+        count = "SELECT count(FREQUENCY)\n"
+        pathquery = "SELECT PATH\n"
+        body = (
+            "FROM (SELECT DISTINCT FILEDATA.FILE_PATH AS PATH, COUNT(FILEDATA.FILE_ID) AS FREQUENCY\n"
+            "FROM IDRESULTS\n"
+            "JOIN FILEDATA on IDRESULTS.FILE_ID = FILEDATA.FILE_ID\n"
+            "JOIN IDDATA on IDRESULTS.ID_ID = IDDATA.ID_ID\n"
+            "WHERE (IDDATA.METHOD='Signature' or IDDATA.METHOD='Container')\n"
+            "GROUP BY FILEDATA.FILE_ID\n"
+            "ORDER BY COUNT(FILEDATA.FILE_ID) DESC)\n"
+            "WHERE FREQUENCY > "
+        )
         if paths is False:
-            query = count + body + str(nscount)
-        else:
-            query = pathquery + body + str(nscount)
+            query = "{}{}{}".format(count, body, nscount)
+            return query
+        query = "{}{}{}".format(pathquery, body, nscount)
         return query
 
     def get_rogue_name_paths(self, itemlist):
-        PATHS = """SELECT DISTINCT FILEDATA.FILE_PATH FROM FILEDATA
-                 WHERE NAME IN """
         csv = '","'.join(itemlist)
-        csv = '("' + csv + '")'
-        query = PATHS + csv
+        csv = '("{}")'.format(csv)
+        PATHS = "SELECT DISTINCT FILEDATA.FILE_PATH FROM FILEDATA\n" "WHERE NAME IN "
+        query = "{}{}".format(PATHS, csv)
         return query
 
     def get_rogue_dir_paths(self, itemlist):
-        query = ""
-        if len(itemlist) > 0:
-            PATHS = """SELECT DISTINCT FILEDATA.FILE_PATH FROM FILEDATA
-                    WHERE DIR_NAME IN """
-            csv = '","'.join(itemlist)
-            csv = '("' + csv + '")'
-            query = PATHS + csv
+        if len(itemlist) <= 0:
+            return ""
+        csv = '","'.join(itemlist)
+        csv = '("{}")'.format(csv)
+        PATHS = (
+            "SELECT DISTINCT FILEDATA.FILE_PATH FROM FILEDATA\n" "WHERE DIR_NAME IN "
+        )
+        query = "{}{}".format(PATHS, csv)
         return query
