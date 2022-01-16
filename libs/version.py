ebead7b617bcb566c29960b71f16b13974affc74
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2022-01-16 20:18:00 +0100

    Up version

--- libs/version.py
+++ libs/version.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 
+"""Version.py
+
+Helper class to return the version of this application to the caller.
+"""
+
 
 class AnalysisVersion:
+    """Analysis version class to store version information and return
+    it to the caller.
+    """
 
-    version = "0.6.7-BETA-test"  # need something reasonable here...
+    def __init__(self):
+        """Constructor for the version class."""
+
+        # 0.6.7-BETA - Python 2 only, first iteration.
+        # 1.0.0 - Python 2 and 3, refactor and final Python 2 release.
+        self.version = "1.0.0"
 
     def getVersion(self):
+        """Return version number to the caller."""
         return self.version
